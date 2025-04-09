61e088ca5f7857818cd93ba63089a073f40eea8c
a7med naser <ad0112727@gmail.com>
2025-04-09 19:10:21 +0200

    edit the getAllTransactions function to be nullable

--- src/manager/ManagerActions.kt
+++ src/manager/ManagerActions.kt
@@ -6,6 +6,6 @@ interface ManagerActions {
     fun addTransaction(transaction: Transaction): Boolean
     fun deleteTransaction(id: Int):Boolean
     fun updateTransaction(transaction: Transaction): Boolean
-    fun getAllTransactions(): List<Transaction>
+    fun getAllTransactions(): List<Transaction>?
     fun getTransactionById(id:Int): Transaction?
 }
