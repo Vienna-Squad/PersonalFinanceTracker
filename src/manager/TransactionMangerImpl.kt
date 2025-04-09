661b9e8e0ee7232d51682d0a3538016850924e3a
a7med naser <ad0112727@gmail.com>
2025-04-09 22:46:36 +0200

    add transaction manger implementation

---
+++ src/manager/TransactionMangerImpl.kt
@@ -0,0 +1,29 @@
+package manager
+
+import models.Transaction
+
+class TransactionMangerImpl : TransactionManger {
+    private val transactions = mutableListOf<Transaction>()
+    override fun addTransaction(transaction: Transaction): Boolean {
+        return transactions.add(transaction)
+    }
+
+    override fun deleteTransaction(id: Int): Boolean {
+        return transactions.removeIf { transactionItem -> transactionItem.id == id }
+    }
+
+    override fun updateTransaction(transaction: Transaction): Boolean {
+        val transactionIndex = transactions.indexOf(transaction)
+        return if (transactionIndex != -1) {
+            transactions[transactionIndex] = transaction
+            true
+        } else
+            false
+    }
+
+    override fun getAllTransactions(): List<Transaction>? = transactions
+
+    override fun getTransactionById(id: Int): Transaction? {
+        return transactions.first { it.id == id }
+    }
+}
