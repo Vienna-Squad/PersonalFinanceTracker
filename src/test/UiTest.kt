e15c872187c28d9a36c1949f4dc35ce5e63472b0
Mohannad Ahmed <mohannad199344@gmail.com>
2025-04-09 15:10:28 +0200

    add the testcases of isValidType function

--- src/test/UiTest.kt
+++ src/test/UiTest.kt
@@ -1,5 +1,33 @@
 package test
 
+import models.TransactionType
+import ui.Validator
+
 fun main() {
+    check(
+        name = "when enter a valid and in range number return the right transaction type",
+        expectedResult = Validator.isValidType("1"),
+        correctResult = TransactionType.INCOME
+    )
+    check(
+        name = "when enter an empty string return null",
+        expectedResult = Validator.isValidType(""),
+        correctResult = null
+    )
+    check(
+        name = "when enter a negative number return null",
+        expectedResult = Validator.isValidType("-1"),
+        correctResult = null
+    )
+    check(
+        name = "when enter an out of range number return null",
+        expectedResult = Validator.isValidType("9"),
+        correctResult = null
+    )
+    check(
+        name = "when enter a character return null",
+        expectedResult = Validator.isValidType("a"),
+        correctResult = null
+    )
 
 }
