5650113fe8895dd05e9597fc44938350ed34a37b
Mohannad Ahmed <mohannad199344@gmail.com>
2025-04-09 14:18:00 +0200

    add the showMenuItems to show the opening menu and handle user's selected option

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,37 @@
+import ui.MenuItem
+
 fun main() {
-    println("Hello Develop! FROM DEVELOP")
+    do {
+        val selectedAction = showMenuItems()
+        when (selectedAction) {
+            MenuItem.ADD -> {}
+
+            MenuItem.UPDATE -> {}
+
+            MenuItem.DELETE -> {}
+
+            MenuItem.VIEW -> {}
+
+            MenuItem.SUMMARY -> {}
+
+            MenuItem.INCOMES -> {}
+
+            MenuItem.EXPENSES -> {}
+
+            MenuItem.EXIT -> {}
+        }
+    } while (selectedAction != MenuItem.EXIT)
 }
+
+fun showMenuItems(): MenuItem {
+    println()
+    MenuItem.entries.forEachIndexed { index, action ->
+        println("${index + 1}- ${action.title}")
+    }
+    print("\nplease choose action *\u001B[33menter anything else to exit\u001B[0m*: ")
+    val selectedAction = readln().toIntOrNull() ?: -1
+    println()
+    return selectedAction.toMenuItem()
+}
+
+
