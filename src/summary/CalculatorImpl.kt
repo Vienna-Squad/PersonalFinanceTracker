e1a4e7797f902d1c5aac3f659db05a1f0ceace62
Mohannad Ahmed <mohannad199344@gmail.com>
2025-04-09 22:37:41 +0200

    edit the naming of transaction list in CalculatorImpl

--- src/summary/CalculatorImpl.kt
+++ src/summary/CalculatorImpl.kt
@@ -6,28 +6,28 @@ import models.TransactionType
 import java.time.LocalDate
 
 
-class CalculatorImpl(val transaction:List<Transaction>):Calculator{
+class CalculatorImpl(private val transactions:List<Transaction>):Calculator{
     override fun calculateIncomesReport(): Report {
         val title = "Total of incomes"
-        val incomes = transaction.filter { it.type == TransactionType.INCOME }
+        val incomes = transactions.filter { it.type == TransactionType.INCOME }
         val totalIncome = incomes.sumOf { it.amount }
         return Report(incomes, totalIncome,title)
     }
 
     override fun calculateExpensesReport(): Report {
         val title  = "Total of expenses"
-        val expenses = transaction.filter { it.type == TransactionType.EXPENSE }
+        val expenses = transactions.filter { it.type == TransactionType.EXPENSE }
         val totalExpense = expenses.sumOf { it.amount }
         return Report(expenses, totalExpense,title)
     }
 
     override fun calculateSummaryOfMonth(month: Int): Report {
         var monthTotalSum = 00.0
-        for (transaction in transactionsList) {
+        for (transaction in transactions) {
             if (transaction.type == TransactionType.INCOME) monthTotalSum += transaction.amount
             else monthTotalSum -= transaction.amount
         }
-        val monthlyFilterList = transactionsList.filter { it.date.monthValue == month }
+        val monthlyFilterList = transactions.filter { it.date.monthValue == month }
         val monthlySummary = Report(
             monthlyFilterList,
             monthTotalSum,
--- src/test/TestCalculator.kt
+++ src/test/TestCalculator.kt
@@ -1,32 +1,38 @@
 package test
 
-import models.Report
 import models.Transaction
 import models.TransactionType
 import summary.CalculatorImpl
 import java.time.LocalDate
 
-fun test(){
-    var dumyData= listOf<Transaction>(
+fun main() {
+    test()
+    testSummaryCalculator()
+}
+
+
+fun test() {
+
+    val dumyData = listOf<Transaction>(
         Transaction(LocalDate.of(2024, 4, 1), 1000.0, "Salary", TransactionType.INCOME),
         Transaction(LocalDate.of(2024, 4, 2), 200.0, "Gift", TransactionType.INCOME),
         Transaction(LocalDate.of(2024, 4, 3), 500.0, "Rent", TransactionType.EXPENSE),
         Transaction(LocalDate.of(2024, 4, 4), 100.0, "Food", TransactionType.EXPENSE)
     )
 
-    val calculatorReport=CalculatorImpl(dumyData)
+    val calculatorReport = CalculatorImpl(dumyData)
 
 
     // Valid TestCase
     check(
         name = ("Check for valid Transaction list Size"),
-        expectedResult =calculatorReport.calculateIncomesReport().transactions.size,
-        correctResult =2
+        expectedResult = calculatorReport.calculateIncomesReport().transactions.size,
+        correctResult = 2
     )
     check(
         name = ("Check for valid amount"),
-        expectedResult =calculatorReport.calculateIncomesReport().sum,
-        correctResult =1200.0
+        expectedResult = calculatorReport.calculateIncomesReport().sum,
+        correctResult = 1200.0
     )
     ////////////////////////////////////////////////////
 
@@ -34,28 +40,23 @@ fun test(){
 
     check(
         name = ("Check for wrong amount"),
-        expectedResult =calculatorReport.calculateExpensesReport().sum,
-        correctResult =400.0
+        expectedResult = calculatorReport.calculateExpensesReport().sum,
+        correctResult = 400.0
     )
     check(
         name = ("Check for invalid Transaction list Size"),
-        expectedResult =calculatorReport.calculateExpensesReport().transactions.size,
-        correctResult =5
+        expectedResult = calculatorReport.calculateExpensesReport().transactions.size,
+        correctResult = 5
     )
 
     check(
         name = ("Empty transaction list"),
-        expectedResult =calculatorReport.calculateExpensesReport().transactions.size,
-        correctResult =0,
+        expectedResult = calculatorReport.calculateExpensesReport().transactions.size,
+        correctResult = 0,
     )
 
 }
 
-
-fun main(){
-  test()
-  testSummaryCalculator()
-}
 
 fun testSummaryCalculator() {
     val dummyTransactions = listOf(
@@ -69,12 +70,12 @@ fun testSummaryCalculator() {
             amount = 1500.0,
             category = "Health",
             type = TransactionType.EXPENSE
-        ) , Transaction(
+        ), Transaction(
             date = LocalDate.now(),
             amount = 500.0,
             category = "Freelance",
             type = TransactionType.INCOME
-        ) , Transaction(
+        ), Transaction(
             date = LocalDate.now(),
             amount = 500.0,
             category = "Clothes",
@@ -95,9 +96,9 @@ fun testSummaryCalculator() {
     )
 //endregion
 //region invalid transaction count of month
-    val dummyTransactions2 = listOf (
+    val dummyTransactions2 = listOf(
         Transaction(
-            date = LocalDate.of(2025,3,28),
+            date = LocalDate.of(2025, 3, 28),
             amount = 2000.0,
             category = "Monthly Salary",
             type = TransactionType.INCOME
@@ -106,12 +107,12 @@ fun testSummaryCalculator() {
             amount = 1500.0,
             category = "Health",
             type = TransactionType.EXPENSE
-        ) , Transaction(
+        ), Transaction(
             date = LocalDate.now(),
             amount = 500.0,
             category = "Freelance",
             type = TransactionType.INCOME
-        ) , Transaction(
+        ), Transaction(
             date = LocalDate.now(),
             amount = 500.0,
             category = "Clothes",
@@ -123,7 +124,7 @@ fun testSummaryCalculator() {
     //invalid
     check(
         name = "Wrong output of  Filtered By Month Transactions ",
-        expectedResult = calculator2.calculateSummaryOfMonth(4).transactions.size ,
+        expectedResult = calculator2.calculateSummaryOfMonth(4).transactions.size,
         correctResult = 3
     )
     //endregion
