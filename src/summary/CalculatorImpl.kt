63a2ef7a90fca197328c90188aedd13814328e6e
lidya rizkallah <lidya.rezk17@gemail.com>
2025-04-09 19:24:46 +0200

    add implemintaion fun calculateIncomesReport add implemintaion fun calculateExpensesReport

--- src/summary/CalculatorImpl.kt
+++ src/summary/CalculatorImpl.kt
@@ -2,6 +2,8 @@ package summary
 
 import models.Report
 import models.Transaction
+import models.TransactionType
+
 
 class CalculatorImpl(val transaction:List<Transaction>):Calculator{
 
@@ -11,12 +13,17 @@ class CalculatorImpl(val transaction:List<Transaction>):Calculator{
     }
 
     override fun calculateIncomesReport(): Report {
-        return Report()
+        val title = "Total of incomes"
+        val incomes = transaction.filter { it.type == TransactionType.INCOME }
+        val totalIncome = incomes.sumOf { it.amount }
+        return Report(incomes, totalIncome)
     }
 
     override fun calculateExpensesReport(): Report {
-        return Report()
+        val title  = "Total of expenses"
+        val expenses = transaction.filter { it.type == TransactionType.EXPENSE }
+        val totalExpense = expenses.sumOf { it.amount }
+        return Report(expenses, totalExpense)
     }
 
-
-}
+    }
