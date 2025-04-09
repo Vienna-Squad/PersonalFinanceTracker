4f2f3c7f309f2f00bce133fa57925595b3f9c600
mohamedshemees <mohamedessampd@gmail.com>
2025-04-09 18:27:48 +0200

    add test case and implementation of calculateSummaryOfMonth

--- src/summary/CalculatorImpl.kt
+++ src/summary/CalculatorImpl.kt
@@ -2,10 +2,24 @@ package summary
 
 import models.Report
 import models.Transaction
+import models.TransactionType
+import java.time.LocalDate
 
-class CalculatorImpl(transaction: List<Transaction>): Calculator {
+class CalculatorImpl(val transactionsList: List<Transaction>) : Calculator {
     override fun calculateSummaryOfMonth(month: Int): Report {
-        return Report()
+        var monthTotalSum = 00.0
+        for (transaction in transactionsList) {
+            if (transaction.type == TransactionType.INCOME) monthTotalSum += transaction.amount
+            else monthTotalSum -= transaction.amount
+        }
+        val monthlyFilterList = transactionsList.filter { it.date.monthValue == month }
+        val monthlySummary = Report(
+            monthlyFilterList,
+            monthTotalSum,
+            "Summary of ${LocalDate.now().month} Transactions "
+
+        )
+        return monthlySummary
     }
 
     override fun calculateIncomesReport(): Report {
--- src/test/TestCalculator.kt
+++ src/test/TestCalculator.kt
@@ -1,6 +1,5 @@
 package test
 
-import models.Report
 import models.Transaction
 import models.TransactionType
 import summary.CalculatorImpl
@@ -8,12 +7,10 @@ import java.time.LocalDate
 
 fun main() {
 testSummaryCalculator()
-
-
 }
 
 fun testSummaryCalculator() {
-    val dummyTransaction = listOf(
+    val dummyTransactions = listOf(
         Transaction(
             date = LocalDate.now(),
             amount = 2000.0,
@@ -36,25 +33,50 @@ fun testSummaryCalculator() {
             type = TransactionType.EXPENSE
         )
     )
-
-    val calculator = CalculatorImpl(dummyTransaction)
-    //valid
+    val calculator = CalculatorImpl(dummyTransactions)
+//region valid transactions
     check(
-        name = "valid summary of size ",
+        name = "Valid Summary Filtered By Month Transactions ",
         expectedResult = calculator.calculateSummaryOfMonth(4).transactions.size,
-        correctResult = dummyTransaction.size
+        correctResult = dummyTransactions.size
     )
     check(
-        name = "balance of INCOME & EXPENSE",
+        name = "Valid Calculated Balance of Income & Expense ",
         expectedResult = calculator.calculateSummaryOfMonth(4).sum,
         correctResult = 500.0
     )
-    //invalid
-    check(
-        name = "invalid summary of sum",
-        expectedResult = calculator.calculateSummaryOfMonth(4).sum ,
-        correctResult = 0.0
+//endregion
+//region invalid transaction count of month
+    val dummyTransactions2 = listOf (
+        Transaction(
+            date = LocalDate.of(2025,3,28),
+            amount = 2000.0,
+            category = "Monthly Salary",
+            type = TransactionType.INCOME
+        ), Transaction(
+            date = LocalDate.now(),
+            amount = 1500.0,
+            category = "Health",
+            type = TransactionType.EXPENSE
+        ) , Transaction(
+            date = LocalDate.now(),
+            amount = 500.0,
+            category = "Freelance",
+            type = TransactionType.INCOME
+        ) , Transaction(
+            date = LocalDate.now(),
+            amount = 500.0,
+            category = "Clothes",
+            type = TransactionType.EXPENSE
+        )
     )
 
-
+    val calculator2 = CalculatorImpl(dummyTransactions2)
+    //invalid
+    check(
+        name = "Wrong output of  Filtered By Month Transactions ",
+        expectedResult = calculator2.calculateSummaryOfMonth(4).transactions.size ,
+        correctResult = 3
+    )
+    //endregion
 }
