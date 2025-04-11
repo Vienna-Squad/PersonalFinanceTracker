8df6bdff5c372cb762fe540115de48bb0221bd64
mohamedshemees <mohamedessampd@gmail.com>
2025-04-11 22:11:19 +0200

    test github actions extracting TransactionType into a file

--- src/TestRunner.kt
+++ src/TestRunner.kt
@@ -1,13 +1,9 @@
 import kotlin.system.exitProcess
 
 fun main() {
-    if(!Test.run()){
+
         println("Test failed.")
         println("test githuba ctions")
         exitProcess(1)
-
     }
-    println("All tests passed.")
-    }
-
 
