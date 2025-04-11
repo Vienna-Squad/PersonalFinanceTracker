93cb43caa6a14fccc5964a17c435bdbc95d77b00
a7med naser <ad0112727@gmail.com>
2025-04-11 20:28:47 +0200

    update file storage

--- src/storage/FileStorageManger.kt
+++ src/storage/FileStorageManger.kt
@@ -20,15 +20,15 @@ class FileStorageManger : Storage {
         file.readLines().forEach { line ->
             val newline = line.split(" | ")
             val item = TransactionModel(
-                id = newline[0].toInt(),
-                date = LocalDate.parse(newline[1]),
-                category = newline[2],
-                amount = newline[3].toDouble(),
-                type = TransactionType.valueOf(newline[4])
+                id = newline[ID_INDEX].toInt(),
+                date = LocalDate.parse(newline[DATE_INDEX]),
+                category = newline[CATEGORY_INDEX],
+                amount = newline[AMOUNT_INDEX].toDouble(),
+                type = TransactionType.valueOf(newline[TYPE_INDEX])
             )
             transactions.add(item)
         }
-        IdGenerator.setInitialValue(transactions.lastOrNull()?.id?.plus(1)?:0)
+        IdGenerator.setInitialValue(transactions.lastOrNull()?.id?.plus(ID_INCREMENT)?:DEFAULT_ID_VALUE)
 
         return transactions
     }
@@ -37,6 +37,17 @@ class FileStorageManger : Storage {
         val stringBuilderFile: StringBuilder = StringBuilder()
         transactions.forEach { item -> stringBuilderFile.append("$item\n") }
         file.writeText(stringBuilderFile.toString())
+    }
+
+    companion object {
+        private const val ID_INDEX = 0
+        private const val DATE_INDEX = 1
+        private const val CATEGORY_INDEX = 2
+        private const val AMOUNT_INDEX = 3
+        private const val TYPE_INDEX = 4
+
+        private const val ID_INCREMENT = 1
+        private const val DEFAULT_ID_VALUE = 0
     }
 
 }
